0734c84a05ada5cf8deaabc64e24a9886a7de024
ainhoajauregiberri <ainhoaillarramendi@opendeusto.es>
2022-11-25 11:18:53 +0100

    comented methods post

--- jurassicpark/main.py
+++ jurassicpark/main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 # Para ejecutar servidor --> uvicorn JurasicPark:app --reload
 
@@ -30,12 +30,12 @@ async def get_dinosaurios(skip: int = 0, limit: int = 10, db: Session = Depends(
 async def get_dinosaurio(nombre: str, db: Session = Depends(get_db)):
     return crud.get_dinosaurio(db, nombre=nombre)
 
-@app.post("/dinosaurio/create", response_model=schemas.DinosaurioCreate)
+"""@app.post("/dinosaurio/create", response_model=schemas.DinosaurioCreate)
 async def create_dinosaurios(dinosaurio: schemas.DinosaurioCreate, db: Session = Depends(get_db)):
     db_dinosaurio = crud.get_dinosaurio_by_nombre(db, nombre = dinosaurio.nombre)
     if db_dinosaurio:
         raise HTTPException(status_code=400, detail="Dinosaurio already registered")
-    return crud.create_dinosaurio(db=db,dinosaurio=dinosaurio)
+    return crud.create_dinosaurio(db=db,dinosaurio=dinosaurio)"""
 
 @app.get("/todoterrenos", response_model=list[schemas.Todoterreno])
 async def get_todoterrenos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
@@ -45,23 +45,23 @@ async def get_todoterrenos(skip: int = 0, limit: int = 10, db: Session = Depends
 async def get_todoterreno(codigo: int, db: Session = Depends(get_db)):
     return crud.get_todoterreno(db, codigo=codigo)
 
-@app.post("/todoterreno/create", response_model=schemas.TodoterrenoCreate)
+"""@app.post("/todoterreno/create", response_model=schemas.TodoterrenoCreate)
 async def create_todoterrenos(todoterreno: schemas.TodoterrenoCreate, db: Session = Depends(get_db)):
     db_todoterreno = crud.get_todoterreno_by_codigo(db, codigo = todoterreno.codigo)
     if db_todoterreno:
         raise HTTPException(status_code=400, detail="Todoterreno already registered")
-    return crud.create_todoterreno(db=db,todoterreno=todoterreno)
+    return crud.create_todoterreno(db=db,todoterreno=todoterreno)"""
 
 @app.get("/especie/{especie}", response_model=schemas.Especie)
 async def get_especie(especie: str, db: Session = Depends(get_db)):
     return crud.get_epecie(db, especie=especie)
 
-@app.post("/especie/create", response_model=schemas.EspecieCreate)
+"""@app.post("/especie/create", response_model=schemas.EspecieCreate)
 async def create_especies(especie: schemas.EspecieCreate, db: Session = Depends(get_db)):
     db_especie = crud.get_especie_by_especie(db, especie = especie.especie)
     if db_especie:
         raise HTTPException(status_code=400, detail="Especie already registered")
-    return crud.create_especie(db=db,especie=especie)
+    return crud.create_especie(db=db,especie=especie)"""
 
 @app.get("/recintos", response_model=list[schemas.Recinto])
 async def get_recintos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
@@ -71,12 +71,12 @@ async def get_recintos(skip: int = 0, limit: int = 10, db: Session = Depends(get
 async def get_recinto(nombre: str, db: Session = Depends(get_db)):
     return crud.get_recinto(db, nombre=nombre)
 
-@app.post("/recinto/create", response_model=schemas.RecintoCreate)
+"""@app.post("/recinto/create", response_model=schemas.RecintoCreate)
 async def create_especies(recinto: schemas.RecintoCreate, db: Session = Depends(get_db)):
     db_recinto = crud.get_recinto_by_nombre(db, recinto = recinto.nombre)
     if db_recinto:
         raise HTTPException(status_code=400, detail="Recinto already registered")
-    return crud.create_recinto(db=db,recinto=recinto)
+    return crud.create_recinto(db=db,recinto=recinto)"""
 
 @app.get("/alarma")
 async def get_alarma(db: Session = Depends(get_db)):
